ba653127aebce61eebbf0e5cbdd2f45797888102
leedahee <ekgml7584@naver.com>
2023-07-13 13:59:53 +0900

    추천 기능

--- setting/views.py
+++ setting/views.py
@@ -6,26 +6,34 @@ from rest_framework.decorators import api_view,permission_classes
 from rest_framework.authentication import BasicAuthentication,SessionAuthentication
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-
-
-# 사용자가 최근에 본 게시물
-# @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# def currentPost(request,pk):
-#     user=get_object_or_404(user)
 
 
 # 사용자의 관심사 기반 추천 게시물 출력
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def recommend(request):
+    bookposts=[]
+    # get 요청한 사용자의 profile을 가져옴.
     profile = Profile.objects.get(user=request.user)
-    interest=profile.interest
-    if len(interest)>0:
-        for i in range(len(interest)):
-            if interest[i] in bookPost.tags:
-                interest_post=bookPost.objects.filter(tags__name=interest[i])
-                serializer=bookPostSerializer(interest_post,many=True)
-                return Response(serializer.data)
 
+    #tags=book_posts.tags
+    # for post in book_posts:
+    #     print(post.tags.all())
+    # #print(book_posts)
+
+    #배열로 반환
+    interest = profile.interest
+    interest = eval(interest)
+
+    #print(interest[0])
+    #print(len(interest))
+    if len(interest)>0:
+        for i in interest:
+            filtered_posts=bookPost.objects.filter(tags__name=i)
+            #print(filtered_posts)
+            bookposts.extend(filtered_posts)
+
+        serializer = bookPostSerializer(bookposts, many=True)
+        return Response(serializer.data)
