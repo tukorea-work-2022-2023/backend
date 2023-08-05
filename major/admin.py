d76b672ef723caf5084d40b8800baa1c51a7a3a4
leedahee <ekgml7584@naver.com>
2023-08-06 00:11:34 +0900

    관리자 페이지 커스텀#2

--- major/admin.py
+++ major/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import bookPost, bookComment, Study, UserRental
 
-# 관리자 페이지에서 관리
-admin.site.register(bookPost)
-admin.site.register(bookComment)
-admin.site.register(Study)
-admin.site.register(UserRental)
+# Register your models here.
